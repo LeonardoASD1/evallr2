ac5e8d5b8bc5a70aa6141a890f1c549841a84413
Leonardo Rivas <95115851+LeonardoASD1@users.noreply.github.com>
2022-10-27 07:58:30 -0400

    Delete evallr2 directory

--- evallr2/funciones.js
+++
@@ -1,8 +0,0 @@
-function limpiar() {
-    document.getElementById('').reset
-}
-
-function sumar(){
-    var x = parseInt (document.getElementById('valores').value);
-    document.getElementById('la-suma').innerHTML= x
-}
